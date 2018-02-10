240d346acb2cd0da80a49c53a2f2655c925733db
xu.wang <867103701@qq.com>
2018-02-10 13:56:25 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <ChildComponent ref="child"></ChildComponent>
+    <richtext>{{richtext}}</richtext>
+  </div>
+</template>
+
+<script>
+  import ChildComponent from "./components/ChildComponent.vue";
+
+  var globalVue;
+  export default {
+    name: 'App',
+    components: {
+      ChildComponent
+    },
+    data: function () {
+      return {
+        richtext: "App.vue receive msg"
+      }
+    },
+    methods: {},
+    created() {
+      globalVue = this;
+    },
+  }
+
+  window["receiveMsgFromNative"] = function (msg) {
+    globalVue.richtext = "parent receive msg from Android qqx5 webview = " + msg;
+    globalVue.$refs.child.receiveMsgFromParent(msg);
+  }
+//  window["receiveMsgFromNative"] = function () {
+//    var msg = "native webview";
+//    globalVue.richtext = "parent receive msg from Android native webview = " + msg;
+//    globalVue.$refs.child.receiveMsgFromParent(msg);
+//  }
+</script>
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="childComponent">
+    <richtext>
+      this is child component
+    </richtext>
+    <br/>
+    <button>{{childMsg}}</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'childComponent',
+    data: function () {
+      return {
+        childMsg: "This is child component"
+      }
+    },
+    created() {
+    },
+    mounted() {
+    },
+    methods: {
+      receiveMsgFromParent: function (msg) {
+        this.childMsg = "receive msg = " + msg;
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
